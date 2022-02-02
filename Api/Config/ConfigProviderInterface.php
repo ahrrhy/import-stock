4b05a80063073a90d12471341608a2bdb6631817
Stanislav Zhuravel <ahrrhy@gmail.com>
2022-02-02 12:51:30 +0200

    add module configurations

---
+++ Api/Config/ConfigProviderInterface.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CtiDigital\InventoryImport\Api\Config;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+interface ConfigProviderInterface
+{
+    public const MODULE_ENABLED_PATH = 'cti_digital/import/enabled';
+    public const FILE_DIRECTORY_PATH = 'cti_digital/import/directory';
+    public const PROCESSED_FILE_DIRECTORY_PATH = 'cti_digital/import/processed_directory';
+    public const FILE_NAME_PATH = 'cti_digital/import/file_name';
+    public const IMPORT_BATCH_SIZE_PATH = 'cti_digital/import/batch_size';
+
+    /**
+     * Check is module enabled in admin
+     *
+     * @param string $scope
+     * @param null $storeId
+     * @return bool
+     */
+    public function isModuleEnabled(
+        string $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+        $storeId = null
+    ): bool;
+
+    /**
+     * Get import file directory path
+     *
+     * @param string $scope
+     * @param null $storeId
+     * @return string
+     */
+    public function getFileDirectoryPath(
+        string $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+        $storeId = null
+    ): string;
+
+    /**
+     * Get processed file directory
+     *
+     * @param string $scope
+     * @param null $storeId
+     * @return string
+     */
+    public function getProcessedFileDirectoryPath(
+        string $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+        $storeId = null
+    ): string;
+
+    /**
+     * Get import file name from configuration
+     *
+     * @param string $scope
+     * @param null $storeId
+     * @return string
+     */
+    public function getImportFileName(
+        string $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+        $storeId = null
+    ): string;
+
+    /**
+     * Get Import Batch Size
+     *
+     * @param string $scope
+     * @param null $storeId
+     * @return string
+     */
+    public function getImportBatchSize(
+        string $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+        $storeId = null
+    ): string;
+}
---
+++ Model/Config/ConfigProvider.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CtiDigital\InventoryImport\Model\Config;
+
+use CtiDigital\InventoryImport\Api\Config\ConfigProviderInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+/**
+ * Retrieve module configurations
+ */
+class ConfigProvider implements ConfigProviderInterface
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * ConfigProvider constructor.
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isModuleEnabled(
+        string $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+        $storeId = null
+    ): bool {
+        return $this->scopeConfig->isSetFlag(
+            self::MODULE_ENABLED_PATH,
+            $scope,
+            $storeId
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getFileDirectoryPath(
+        string $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+        $storeId = null
+    ): string {
+        return $this->scopeConfig->getValue(
+            self::FILE_DIRECTORY_PATH,
+            $scope,
+            $storeId
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getProcessedFileDirectoryPath(
+        string $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+        $storeId = null
+    ): string {
+        return $this->scopeConfig->getValue(
+            self::PROCESSED_FILE_DIRECTORY_PATH,
+            $scope,
+            $storeId
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getImportFileName(
+        string $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+        $storeId = null
+    ): string {
+        return $this->scopeConfig->getValue(
+            self::FILE_NAME_PATH,
+            $scope,
+            $storeId
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getImportBatchSize(
+        string $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+        $storeId = null
+    ): string {
+        return $this->scopeConfig->getValue(
+            self::IMPORT_BATCH_SIZE_PATH,
+            $scope,
+            $storeId
+        );
+    }
+}
